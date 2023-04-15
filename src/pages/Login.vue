adbed8c63c6dbf5e4cd940c9df98e135509b9dde
Alessandra Avelino <alessavelino123@gmail.com>
2023-04-14 22:56:12 -0300

    Melhoria no layout do login.

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,68 +1,89 @@
 <template>
-  <div class="area">
-    <ul class="circles">
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
+  <div class="half">
+    <div class="area">
+      <ul class="circles">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
 
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
 
-      <li></li>
-    </ul>
+        <li></li>
+      </ul>
 
-    <div
-      class="window-height window-width row justify-around items-center"
-      align="center"
-    >
-      <div>
-        <img src="~assets/logo-login.svg" />
+      <div class="window-height window-width row items-center">
+        <div class="bg-login">
+          <img src="~assets/logo-login.svg" />
+        </div>
       </div>
-      <div class="">
-        <q-card class="my-card q-pa-md card" align="center">
-          <h4>Fazer Login</h4>
-          <q-card-section class="q-gutter-md" style="width: 360px">
-            <q-input
-              filled
-              type="email"
-              v-model="email"
-              required
-              label="E-mail"
-            />
-            <q-input
-              filled
-              v-model="senha"
-              required
-              label="Senha"
-              :type="isPwd ? 'senha' : 'text'"
-            >
-              <template v-slot:append>
-                <q-icon
-                  :name="isPwd ? 'visibility_off' : 'visibility'"
-                  class="cursor-pointer"
-                  @click="isPwd = !isPwd"
-                />
-              </template>
-            </q-input>
-            <q-btn
-              color="primary"
-              @click="login"
-              type="submit"
-              rounded
-              label="Login"
-            />
-          </q-card-section>
-          <q-row class="q-ma-md">
-            <q-col class="col-6 q-pa-md" @click="cadastrarPage"
-              >Cadastrar-se</q-col
-            >
-            <q-col class="col-6 q-pa-md">Esqueci a senha</q-col>
-          </q-row>
-        </q-card>
+    </div>
+  </div>
+  <div class="half">
+    <div class="">
+      <div class="my-card q-pa-md card" align="center">
+        <h4>Fazer Login</h4>
+        <div class="q-gutter-md" style="width: 360px">
+          <q-input
+            filled
+            type="email"
+            v-model="email"
+            required
+            label="E-mail"
+          />
+          <q-space></q-space>
+          <q-input
+            filled
+            v-model="senha"
+            required
+            label="Senha"
+            :type="isPwd ? 'password' : 'text'"
+          >
+            <template v-slot:append>
+              <q-icon
+                :name="isPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPwd = !isPwd"
+              />
+            </template>
+          </q-input>
+          <div style="margin-left: 50%">
+            <p class="textLink">Esqueceu a senha?</p>
+          </div>
+          <q-btn
+            style="width: 310px; height: 25px"
+            color="primary"
+            @click="login"
+            type="submit"
+            label="entrar"
+          >
+            <div v-if="isLoading">
+              <q-spinner-facebook color="white" size="20px" />
+              <q-tooltip :offset="[0, 8]">QSpinnerFacebook</q-tooltip>
+            </div>
+          </q-btn>
+        </div>
+        <q-row class="q-ma-md">
+          <p>Ainda não possui uma conta?</p>
+          <q-col class="textLink" @click="cadastrarPage">Cadastrar-se</q-col>
+        </q-row>
+      </div>
+      <hr />
+      <div class="parceria q-pt-xl">
+        <p>Deseja se tornar parceiro?</p>
+        <q-btn
+          @click="login"
+          type="submit"
+          label="solicitar parceria"
+          class="button-parc"
+          rounded
+        />
       </div>
     </div>
   </div>
@@ -71,20 +92,32 @@
 <script>
 import { ref } from "vue";
 import axios from "axios";
-
+import { Notify } from "quasar";
+import { useQuasar } from "quasar";
 export default {
   name: "Login",
+
   data() {
     return {
       email: "",
       senha: "",
       isPwd: ref(true),
+      isLoading: false,
     };
   },
+
   methods: {
+    showNotif() {
+      this.$q.notify({
+        message: "Credenciais inválidas! Tente novamente!",
+        color: "purple",
+      });
+    },
     async login() {
       console.log("clicado");
+      const $q = useQuasar();
       try {
+        this.isLoading = true;
         const response = await axios.post("http://127.0.0.1:5000/login", {
           email: this.email,
           senha: this.senha,
@@ -95,10 +128,16 @@ export default {
         this.$router.push("/dashboard");
       } catch (error) {
         console.log(error);
-        alert("Credenciais inválidas!");
+        this.$q.notify({
+          message: "Credenciais inválidas! Tente novamente!",
+          color: "red",
+        });
+      } finally {
+        this.isLoading = false;
       }
     },
     cadastrarPage() {
+      console.log('lcicado')
       this.$router.push("/cadastrar");
     },
   },
@@ -109,21 +148,28 @@ export default {
 * {
   overflow-y: hidden;
   overflow-x: hidden;
+  overflow: hidden;
 }
 
 .area {
   background: #4e54c8;
   background: -webkit-linear-gradient(to left, #8f94fb, #4e54c8);
+  overflow: hidden;
 }
 
+
 .circles {
-  position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 98.3%;
+  z-index: 0;
+  overflow: hidden;
 }
 
+.bg-login {
+  z-index: 2;
+}
 .circles li {
   position: absolute;
   display: block;
@@ -131,8 +177,9 @@ export default {
   width: 20px;
   height: 20px;
   background: rgba(255, 255, 255, 0.2);
-  animation: animate 25s linear infinite;
-  bottom: -150px;
+  animation: animate 10s linear infinite;
+  bottom: -10px;
+  overflow: hidden;
 }
 
 .circles li:nth-child(1) {
@@ -140,14 +187,19 @@ export default {
   width: 80px;
   height: 80px;
   animation-delay: 0s;
+  overflow: hidden;
 }
 
+.bg-img {
+  margin: 0 auto; /* centraliza horizontalmente */
+}
 .circles li:nth-child(2) {
   left: 10%;
   width: 20px;
   height: 20px;
   animation-delay: 2s;
   animation-duration: 12s;
+  overflow: hidden;
 }
 
 .circles li:nth-child(3) {
@@ -183,7 +235,7 @@ export default {
   left: 35%;
   width: 150px;
   height: 150px;
-  animation-delay: 7s;
+  animation-delay: 0s;
 }
 
 .circles li:nth-child(8) {
@@ -191,15 +243,15 @@ export default {
   width: 25px;
   height: 25px;
   animation-delay: 15s;
-  animation-duration: 45s;
+  animation-duration: 10s;
 }
 
 .circles li:nth-child(9) {
   left: 20%;
   width: 15px;
   height: 15px;
-  animation-delay: 2s;
-  animation-duration: 35s;
+  animation-delay: 12s;
+  animation-duration: 6s;
 }
 
 .circles li:nth-child(10) {
@@ -223,7 +275,31 @@ export default {
     border-radius: 50%;
   }
 }
-.card {
-  border-radius: 10px;
+.half {
+  width: 50%;
+  height: 100%;
+  float: left;
+  text-align: center;
+  overflow: hidden;
+}
+
+hr {
+  width: 70%;
+}
+
+.button-parc {
+  border: 1px solid black;
+  border-radius: 20;
+  border-width: 1px;
+  padding: 10px 20px;
+  background-color: white;
+  color: black;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+.textLink {
+  color: #0775f3;
+  cursor: pointer;
 }
 </style>
