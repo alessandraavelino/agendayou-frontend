2768d3d2bef090d2288cb654dffeb35cb005f010
Alessandra Avelino <alessavelino123@gmail.com>
2023-06-10 13:00:28 -0300

    Página de relatórios/faturamento

--- src/pages/Parceiros/Relatorio.vue
+++ src/pages/Parceiros/Relatorio.vue
@@ -3,175 +3,42 @@
       <div class="q-pa-md">
         <div class="row q-pb-md">
           <div>
-            <span class="col-4">Cadastrar Serviços</span>
-          </div>
-          <div class="col row justify-end">
-            <q-btn
-              label="add"
-              icon="add_circle"
-              rounded
-              class=".col-md-4 .offset-md-4 btnAdd"
-              color="primary"
-              @click="openDialog"
-            />
+            <span class="col-4">Relatório de Atendimentos</span>
           </div>
         </div>
         <q-table
-          title="Serviços cadastrados"
-          :rows="rows"
+          title="Total"
+          :rows="filteredRows"
           :columns="columns"
-          row-key="id_servico"
         >
-          <template v-slot:body-cell-acoes="{ row }">
-            <div class="text-center q-pt-sm icons">
-              <q-badge
-                class="icons_delete_update"
-                rounded
-                color="green"
-                clickable
-                @click="editarItem(row)"
-              >
-                <q-icon name="edit" clickable size="15px"></q-icon>
-              </q-badge>
-              <q-badge
-                class="icons_delete_update"
-                rounded
-                color="red"
-                clickable
-                @click="deleteItem(row)"
-              >
-                <q-icon name="delete_outline" clickable size="15px"></q-icon>
-              </q-badge>
-            </div>
+        <template v-slot:top-right>
+        <q-input borderless dense debounce="300" style="border-bottom: 1px solid #ccc;" v-model="buscar" placeholder="Buscar...">
+          <template v-slot:append>
+            <q-icon name="search" />
           </template>
-        </q-table>
-      </div>
-      <template>
-        <div class="q-pa-md q-gutter-md">
-          <q-badge rounded color="yellow" />
-          <q-badge rounded color="green" />
-          <q-badge rounded color="red" />
-          <div class="q-gutter-md q-ml-none">
-            <q-btn round icon="notifications">
-              <q-badge floating color="red" rounded />
-            </q-btn>
-            <q-btn color="blue">
-              Notifications
-              <q-badge color="red" rounded floating />
-            </q-btn>
-          </div>
-          <div><q-badge color="blue" rounded class="q-mr-sm" />Status</div>
-        </div>
+        </q-input>
       </template>
-  
-      <q-dialog v-model="alert" persistent>
-        <q-card>
-          <q-card-section>
-            <div class="text-h6">Preencha os dados abaixo:</div>
-          </q-card-section>
-  
-          <q-card-section class="q-pt-none" style="width: 500px">
-            <div class="q-gutter-md">
-              <q-input v-model="fields.tipo_servico" label="Serviço" required/>
-              <q-input v-model="fields.profissional" label="Profissional" required />
-              <q-input v-model="fields.valor" label="Valor" type="number" required />
-              <div class="row">
-                <div class="q-gutter-md" style="max-width: 300px">
-                  <q-input
-                    filled
-                    required
-                    v-model="fields.horario"
-                    hint="Selecione a data e hora"
-                  >
-                    <template v-slot:prepend>
-                      <q-icon name="event" class="cursor-pointer">
-                        <q-popup-proxy
-                          cover
-                          transition-show="scale"
-                          transition-hide="scale"
-                        >
-                          <q-date
-                            v-model="fields.horario"
-                            mask="DD/MM/YYYY HH:mm"
-                          >
-                            <div class="row items-center justify-end">
-                              <q-btn
-                                v-close-popup
-                                label="Close"
-                                color="primary"
-                                flat
-                              />
-                            </div>
-                          </q-date>
-                        </q-popup-proxy>
-                      </q-icon>
-                    </template>
-  
-                    <template v-slot:append>
-                      <q-icon name="access_time" class="cursor-pointer">
-                        <q-popup-proxy
-                          cover
-                          transition-show="scale"
-                          transition-hide="scale"
-                        >
-                          <q-time
-                            v-model="fields.horario"
-                            mask="DD/MM/YYYY HH:mm"
-                            format24h
-                          >
-                            <div class="row items-center justify-end">
-                              <q-btn
-                                v-close-popup
-                                label="Close"
-                                color="primary"
-                                flat
-                              />
-                            </div>
-                          </q-time>
-                        </q-popup-proxy>
-                      </q-icon>
-                    </template>
-                  </q-input>
-                </div>
-              </div>
-            </div>
-          </q-card-section>
-  
-          <q-card-actions align="right">
-            <q-btn flat label="Cancelar" color="negative" v-close-popup />
-            <q-btn flat label="Limpar" @click="cleanFields" color="clear" />
-            <q-btn
-              flat
-              label="Salvar"
-              type="button"
-              @click="cadastrarServico()"
-              :disable="!fields.horario || !fields.profissional || !fields.valor || !fields.tipo_servico"
-              color="primary"
-              v-close-popup
-            />
-          </q-card-actions>
-        </q-card>
-      </q-dialog>
-      <q-dialog v-model="confirmDialogVisible" persistent>
-        <q-card>
-          <q-card-section>
-            <div class="text-h6">Tem certeza que deseja excluir o item?</div>
-          </q-card-section>
-  
-          <q-card-actions align="right">
-            <q-btn flat label="Cancelar" color="gray" v-close-popup />
-            <q-btn
-              flat
-              label="Excluir"
-              @click="excluirItem"
-              color="negative"
-              v-close-popup
-            />
-          </q-card-actions>
-        </q-card>
-      </q-dialog>
-      <div class="flex flex-center" v-if="isLoading">
-        <q-spinner color="primary" size="3em" />
+        </q-table>
+        <div class="q-pa-md q-gutter-md row">
+          <div class="text-h6">
+            Clientes Atendidos:
+            <q-badge outline align="middle" class="dados q-pa-sm" color="primary">
+              {{ presente }}
+            </q-badge>
+          </div>
+          <div class="text-h6">
+            Clientes Ausentes:
+            <q-badge outline align="middle" class="dados q-pa-sm" color="orange">
+              {{ ausente }}
+            </q-badge>
+          </div>
+          <div class="text-h6">
+            Total de Lucro:
+            <q-badge outline align="middle" class="dados q-pa-sm" color="green">
+              R$ {{ valor }}
+            </q-badge>
+          </div>
+        </div>
       </div>
     </q-page>
   </template>
@@ -180,11 +47,18 @@
   import { defineComponent } from "vue";
   const columns = [
     {
-      name: "id",
+      name: "nome_cliente",
       required: true,
       align: "center",
-      label: "ID",
-      field: "id_servico",
+      label: "Cliente",
+      field: "nome_cliente",
+      sortable: false,
+    },
+    {
+      name: "telefone",
+      align: "center",
+      label: "Telefone",
+      field: "telefone",
       sortable: false,
     },
     {
@@ -209,20 +83,19 @@
       sortable: false,
     },
     {
+      name: "status",
+      align: "center",
+      label: "Status",
+      field: "status",
+      sortable: false,
+    },
+    {
       name: "valor",
       align: "center",
       label: "Valor",
       field: "valor",
       sortable: false,
-    },
-    {
-      name: "acoes",
-      align: "center",
-      label: "Ações",
-      field: "acoes",
-      sortable: false,
-      class: "q-table__cell-no-number",
-    },
+    }
   ];
   
   import { ref } from "vue";
@@ -234,13 +107,11 @@
     name: "CadastrarServicos",
     data() {
       return {
-        fields: {
-          id_servico: "",
-          tipo_servico: "",
-          profissional: "",
-          valor: "",
-          horario: null,
-        },
+        buscar: "",
+        filtro: "",
+        ausente: "",
+        presente: "",
+        valor: "",
         isLoading: false,
         id_servico: "",
         columns,
@@ -249,142 +120,80 @@
         servico: "",
         profissional: "",
         time: "",
-        valor: "",
         rows: [],
         confirmDialogVisible: false,
       };
     },
   
     mounted() {
-      this.getServicos();
+      this.getRelatorio();
+      this.getFaturamentoTotal();
+    },
+
+    computed: {
+      filteredRows() {
+      // Filtra os dados com base no valor digitado no campo de busca
+        const searchTerm = this.buscar.toLowerCase();
+        return this.rows.filter(row => {
+          // Verifica se algum valor da linha contém o termo de busca
+          return Object.values(row).some(value => {
+            return String(value).toLowerCase().includes(searchTerm);
+          });
+        });
+      }
     },
   
     methods: {
-      async cadastrarServico() {
-        const $q = useQuasar();
-  
-        const data = {
-          nome_parceiro: localStorage.getItem("nome"),
-          categoria: localStorage.getItem("categoria"),
-          parceiro_id: localStorage.getItem("id_parceiro"),
-          tipo_servico: this.fields.tipo_servico,
-          profissional: this.fields.profissional,
-          valor: this.fields.valor,
-          horario: this.fields.horario,
-        };
-  
-        if (this.fields.id_servico) {
-          console.log("caiu aquiu");
-          console.log("id_servic", this.fields.id_servico);
-          try {
-            const response = await axios.put(
-              `http://127.0.0.1:5000/servicos/${this.fields.id_servico}`,
-              data
-            );
-            console.log("response", response);
-            this.getServicos();
-          } catch (error) {
-            console.log(error);
-          }
-        } else {
-          this.cleanFields();
-          try {
-            this.isLoading = true;
-  
-            const response = await axios.post(
-              "http://127.0.0.1:5000/servicos",
-              data
-            );
-            this.alert = false;
-            this.getServicos();
-          } catch (error) {
-            console.log(error);
-            this.$q.notify({
-              message: "Ocorreu um erro! Tente novamente!",
-              color: "red",
-            });
-            this.getServicos();
-          } finally {
-            this.isLoading = false;
-          }
-        }
-      },
-  
-      async editarItem(row) {
-        this.fields.id_servico = row.id_servico;
-        // Preencha os campos do formulário com os dados do item a ser editado
-        this.fields.tipo_servico = row.tipo_servico;
-        this.fields.profissional = row.profissional;
-        this.fields.valor = row.valor;
-        this.fields.horario = row.horario;
-  
-        // Abra o q-dialog de alerta
-        this.alert = true;
-      },
-  
-      cleanFields() {
-        this.fields.id_servico = "";
-        this.fields.tipo_servico = "";
-        this.fields.profissional = "";
-        this.fields.valor = "";
-        this.fields.horario = "";
-        this.alert = true;
-      },
-  
-      openDialog() {
-        this.cleanFields();
-        this.alert = true;
-      },
-  
-      deleteItem(row) {
-        // Defina o item que será excluído
-        this.deleteItemId = row.id_servico;
-  
-        // Abra o diálogo de confirmação
-        this.confirmDialogVisible = true;
-      },
-  
-      async getServicos() {
+      async getRelatorio() {
         this.loading = true;
         const parceiro_id = localStorage.getItem("id_parceiro");
         console.log("parceiro id", parceiro_id);
-        const url = `http://127.0.0.1:5000/servicos/${parceiro_id}`;
+        const url = `http://127.0.0.1:5000/faturamentos/${parceiro_id}`;
         try {
           const response = await axios.get(url);
           console.log("reponse", response);
           const servicos = response.data.map((el) => ({
-            id_servico: el.id_servico,
+            id_faturamento: el.id_faturamento,
+            status: el.status,
+            nome_cliente: el.nome_cliente,
+            telefone: el.telefone,
             tipo_servico: el.tipo_servico,
-            horario: el.horario,
             profissional: el.profissional,
-            valor: el.valor,
+            horario: el.horario,
+            valor: el.valor
           }));
-  
-          console.log("servic", servicos);
-  
+
+          servicos.forEach(el => {
+            if (el.status === 1) {
+              el.status = 'Presente'
+            }
+            if (el.status === 0) {
+              el.status = 'Ausente'
+            }
+          })
           this.rows = servicos;
         } catch (error) {
           console.log(error);
         }
       },
-      async excluirItem() {
-        this.isLoading = true;
-        const url = `http://127.0.0.1:5000/servicos/${this.deleteItemId}`;
+      async getFaturamentoTotal() {
+        this.loading = true;
+        const parceiro_id = localStorage.getItem("id_parceiro");
+        console.log("parceiro id", parceiro_id);
+        const url = `http://127.0.0.1:5000/faturamento/${parceiro_id}`;
         try {
-          await axios.delete(url);
-  
-          this.$q.notify({
-            message: "Serviço excluído com sucesso!",
-            color: "green",
-          });
-          this.getServicos();
+          const response = await axios.get(url);
+          console.log("reponse", response);
+          this.ausente = response.data.ausente
+          this.presente = response.data.presente
+          this.valor = response.data.valor
+
         } catch (error) {
           console.log(error);
-        } finally {
-          this.isLoading = false;
         }
       },
-    },
+
+    }
   });
   </script>
   
@@ -392,15 +201,10 @@
   span {
     font-size: 20px;
   }
-  .btnAdd {
-    font-weight: 900;
-  }
-  .icons_delete_update {
-    cursor: pointer;
-  }
-  
-  .icons_delete_update:hover {
-    color: rgb(79, 79, 79);
+
+  .dados {
+    font-size: 20px;
+    size: 50px;
   }
   </style>
   
